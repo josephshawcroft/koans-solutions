76e8d880020f7b53e92593f5a6422e2b0c40918c
Joseph Shawcroft <joeymoto@hotmail.co.uk>
2018-12-08 22:41:41 +0000

    Added test stuff

--- src/ii_collections/n22Fold.kt
+++ src/ii_collections/n22Fold.kt
@@ -15,7 +15,5 @@ fun whatFoldDoes(): Int {
 fun Shop.getSetOfProductsOrderedByEachCustomer(): Set<Product> {
     // Return the set of products that were ordered by each of the customers
     return customers.fold(allOrderedProducts, {
-        orderedByAll, customer ->
-        todoCollectionTask()
-    })
+        orderedByAll, customer -> allOrderedProducts.intersect(allOrderedProducts)
 }
