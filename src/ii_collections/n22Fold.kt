68275e2d376607c230862f82c3c1b5bd193de8c6
Joseph Shawcroft <Joe@Josephs-MacBook-Pro.local>
2018-12-09 18:40:01 +0000

    Added fold solution

--- src/ii_collections/n22Fold.kt
+++ src/ii_collections/n22Fold.kt
@@ -15,5 +15,5 @@ fun whatFoldDoes(): Int {
 fun Shop.getSetOfProductsOrderedByEachCustomer(): Set<Product> {
     // Return the set of products that were ordered by each of the customers
     return customers.fold(allOrderedProducts, {
-        orderedByAll, customer -> allOrderedProducts.intersect(allOrderedProducts)
+        orderedByAll, customer -> orderedByAll.intersect(customer.orderedProducts)})
 }
