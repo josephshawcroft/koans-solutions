d5ad3e4c312913bb774c33f23975b9e8dd470b9d
CORP\joseph.shawcroft <joseph.shawcroft@ert.com>
2018-12-10 15:00:33 +0000

    Added lazy property

--- src/iv_properties/n32Properties.kt
+++ src/iv_properties/n32Properties.kt
@@ -3,7 +3,7 @@ package iv_properties
 import util.TODO
 import util.doc32
 
-class PropertyExample() {
+class PropertyExample {
     var counter = 0
     var propertyWithCounter: Int? = 0
         set(value) {
