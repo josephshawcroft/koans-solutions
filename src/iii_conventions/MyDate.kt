67a5a727f8047db3504e20e93fe12e648ab603f0
CORP\joseph.shawcroft <joseph.shawcroft@ert.com>
2018-12-10 11:02:08 +0000

    Finished q29

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -12,16 +12,33 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
     }
 }
 
-operator fun DateRange.contains(other: MyDate) : Boolean {
+operator fun DateRange.contains(other: MyDate): Boolean {
     return other >= this.start && other <= this.endInclusive
 }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange {
+    return DateRange(this, other)
+}
 
 enum class TimeInterval {
     DAY,
     WEEK,
     YEAR
 }
+
+operator fun DateRange.iterator(): Iterator<MyDate> = object : Iterator<MyDate> {
+    var current = start
+
+    override fun hasNext(): Boolean {
+        return current <= endInclusive
+    }
+
+    override fun next(): MyDate {
+        return current.apply {
+            current = current.nextDay()
+        }
+    }
+}
+
 
 class DateRange(val start: MyDate, val endInclusive: MyDate)
