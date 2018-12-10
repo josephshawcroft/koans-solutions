bebaee5eb43c080f652dddd976262b92529bfdcf
Joseph Shawcroft <joeymoto@hotmail.co.uk>
2018-12-10 19:58:16 +0000

    Added extension functions

--- src/v_builders/n37StringAndMapBuilders.kt
+++ src/v_builders/n37StringAndMapBuilders.kt
@@ -2,6 +2,8 @@ package v_builders
 
 import util.TODO
 import java.util.*
+import kotlin.collections.AbstractMap
+import kotlin.collections.HashMap
 
 fun buildStringExample(): String {
     fun buildString(build: StringBuilder.() -> Unit): String {
@@ -29,11 +31,17 @@ fun todoTask37(): Nothing = TODO(
 )
 
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+
+    fun buildMap(buildMap: HashMap<Int, String>.() -> Unit): HashMap<Int, String> {
+        val hashmap = HashMap<Int, String>()
+        hashmap.buildMap()
+        return hashmap
+    }
+
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
 }
